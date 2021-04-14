b7e12c8d66abc26814266ea2ac43fe464900124a
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-04-14 12:32:26 +0900

    タスク作成機能を追加。

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import { FormControl, TextField } from '@material-ui/core'
+import AddToPhotosIcon from '@material-ui/icons/AddToPhotos';
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import { db } from './firebase'
 
 const App:React.FC = ()=> {
   const [tasks, setTasks] = useState([{ id: "", title: "" }]);
+  const [input, setInput] = useState("");
+
   useEffect(() => {
     const unSub = db.collection("tasks").onSnapshot((snapshot) => {
       setTasks(
@@ -13,9 +17,28 @@ const App:React.FC = ()=> {
     return ()=> unSub()
   }, []);
 
+  const newTask = (e: React.MouseEventHandler<HTMLButtonElement>) => {
+    db.collection("tasks").add({ title: input })
+    setInput("")
+  }
+
   return (
     <div className="App">
-      {tasks.map((task) => <h3>{task.title}</h3>)}
+      <h1>Todo App by React/Firebase</h1>
+      <FormControl>
+        <TextField
+          InputLabelProps={{
+            shrink:true
+          }}
+          label="New Task ?"
+          value={input}
+          onChange={(e:React.ChangeEvent<HTMLInputElement>) => setInput(e.target.value)}
+        />
+      </FormControl>
+      <button disabled={!input} onClick={newTask}>
+        <AddToPhotosIcon/>
+      </button>
+      {tasks.map((task) => <h3 key={task.id}>{task.title}</h3>)}
     </div>
   );
 }
