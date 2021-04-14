798e5f36af95d971515182c7a2aba6afa1bb20cc
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-04-15 08:24:12 +0900

    CRUD機能を実装

--- src/TaskItem.tsx
+++ src/TaskItem.tsx
@@ -1,22 +1,49 @@
-import React from 'react'
-import firebase from 'firebase/app'
+import React, { useState } from 'react';
+import firebase from 'firebase/app';
 import { ListItem, TextField, Grid } from '@material-ui/core';
 import DeleteOutlineOutlinedIcon from '@material-ui/icons/DeleteOutlineOutlined';
 import EditOutlinedicon from '@material-ui/icons/EditOutlined';
+import { db } from './firebase';
 
 interface PROPS {
-  id: string
-  title:string
+  id: string;
+  title: string;
 }
 
-const TaskItem:React.FC<PROPS> = (props) => {
+const TaskItem: React.FC<PROPS> = (props) => {
+  const [title, settitle] = useState(props.title);
+
+  const editTask = () => {
+    db.collection('tasks').doc(props.id).set({ title: title }, { merge: true });
+  };
+
+  const deleteTask = () => {
+    db.collection('tasks').doc(props.id).delete();
+  };
+
   return (
-    <div>
-      <ListItem>
-        <h2>{props.title}</h2>
-      </ListItem>
-    </div>
-)
-}
+    <ListItem>
+      <h2>{props.title}</h2>
+      <Grid container justify="flex-end">
+        <TextField
+          InputLabelProps={{
+            shrink: true,
+          }}
+          label="Edit task"
+          value={title}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            settitle(e.target.value)
+          }
+        />
+      </Grid>
+      <button onClick={editTask}>
+        <EditOutlinedicon />
+      </button>
+      <button onClick={deleteTask}>
+        <DeleteOutlineOutlinedIcon />
+      </button>
+    </ListItem>
+  );
+};
 
-export default TaskItem
+export default TaskItem;
