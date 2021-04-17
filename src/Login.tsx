1629251975a40704a29da7eec48cbc993bc9c3a7
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-04-17 22:48:34 +0900

    login認証にメール機能を実装

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import styles from './Login.module.css';
+import { Button, FormControl, TextField, Typography } from '@material-ui/core';
+import { auth } from './firebase';
 
-const login = () => {
-  return <div>Login Component</div>;
+const Login: React.FC = (props: any) => {
+  const [isLogin, setIsLogin] = useState(true);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      user && props.history.push('/');
+    });
+  }, [props.histroy]);
+
+  return (
+    <div>
+      <h1>{isLogin ? 'Login' : 'Register'}</h1>
+      <br />
+      <FormControl>
+        <TextField
+          InputLabelProps={{
+            shrink: true,
+          }}
+          name="email"
+          label="E-mail"
+          value={email}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            setEmail(e.target.value);
+          }}
+        />
+      </FormControl>
+    </div>
+  );
 };
 
-export default login;
+export default Login;
